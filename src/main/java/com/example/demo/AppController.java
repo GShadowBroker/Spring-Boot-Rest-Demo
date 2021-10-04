b7ad92493ea765d14bd572687a72f11fb4a052ec
gledyson <gledysonferreira@gmail.com>
2021-10-03 22:56:29 -0400

    added greeting module

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
-	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello, %s!", name);
+	@GetMapping("/")
+	public String sayHello() {
+		return "Welcome to my awesome little app! Enjoy your stay!";
 	}
 }
